a50f4a4a663186a26fc5e20ee679655f2ea5b52a
DeepakR-28 <awsmyo1234@gmail.com>
2021-12-30 00:37:00 +0530

    updated readme

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const axios = require('axios')
 
 function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2-lat1);  // deg2rad below
-    var dLon = deg2rad(lon2-lon1); 
+    var radius = 6371; // Radius of the earth in km
+    var latitudeDifference = deg2rad(lat2-lat1);  // deg2rad below
+    var longitudeDifference = deg2rad(lon2-lon1); 
     var a = 
-      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.sin(latitudeDifference/2) * Math.sin(latitudeDifference/2) +
       Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-      Math.sin(dLon/2) * Math.sin(dLon/2);
+      Math.sin(longitudeDifference/2) * Math.sin(longitudeDifference/2);
     
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var d = R * c; // Distance in km
-    return d
+    var distance = radius * c; // Distance in km
+    return distance
   }
   
   function deg2rad(deg) {
